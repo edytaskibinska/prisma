04a251ca5bce7e2f1d99f5181d31d21a49cb9f50
Edyta <eskibinska@yolaw.fr>
2020-03-15 20:41:59 +0100

    deleting console log

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ const paralaxing = new DynamicParallax(".parallax1");
 const paralaxingLogo = new DynamicParallax(".parallax2");
 
 window.addEventListener("scroll", function() {
-  console.log(paralaxing.item);
-  this.console.log(window.scrollY);
-  console.log("scrolling");
   paralaxing.parallax(0.8, window.scrollY);
   paralaxingLogo.parallax(0.4, window.scrollY);
 });
@@ -17,6 +14,5 @@ const menuToggle = document.querySelector(".burger");
 
 menuToggle.onclick = function(e) {
   e.preventDefault();
-  console.log(menu);
   menu.classList.toggle("open");
 };
