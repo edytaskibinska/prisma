9b0d475cd1ddf5d840cccdde9bc78dbecfa610f1
Edyta <eskibinska@yolaw.fr>
2020-03-15 15:48:15 +0100

    adding basic configuration

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import bro from "./parallax";
+import "./styles/main.scss";
+
+console.log(bro("Brooo"));
---
+++ src/parallax.js
@@ -0,0 +1,5 @@
+const bro = greet => {
+  return `${greet}, bro`;
+};
+
+export default bro;
